56aa1110dde79a8713d4456559b0fee38a70a08f
Bentil Shadrack <bentilshadrack72@gmail.com>
2021-07-22 16:00:56 +0000

    Distance between two point's Challenge
    Finding Distance between two point's in the XY plane

---
+++ distance between two points/Main.java
@@ -0,0 +1,26 @@
+package academy.Bentilzone;
+
+import java.util.Scanner;
+
+public class Main {
+    public static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        float x1, x2, y1, y2;
+        System.out.print("Enter x1: ");
+        x1 = scanner.nextFloat();
+        System.out.print("Enter y1: ");
+        y1 = scanner.nextFloat();
+        System.out.print("Enter x2: ");
+        x2 = scanner.nextFloat();
+        System.out.print("Enter y2: ");
+        y2 = scanner.nextFloat();
+        System.out.printf("Length of line is: %.3f",lengthOfLine(x1, x2,y1,y2));
+
+    }
+    public static double lengthOfLine(float x1, float x2,float y1,float y2){
+        float side1 = x2-x1;
+        float side2 = y2-y1;
+        double length =  Math.sqrt((side1*side1) + (side2*side2));
+        return length;
+    }
+}
